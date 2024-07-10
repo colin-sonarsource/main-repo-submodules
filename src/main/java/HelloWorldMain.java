78551ac96214654df1e454a7cff8a71222b4e9f4
colin-sonarsource <colin.mueller@sonarsource.com>
2024-07-10 16:38:14 +0200

    Update HelloWorldMain.java

--- src/main/java/HelloWorldMain.java
+++ src/main/java/HelloWorldMain.java
@@ -1,5 +1,5 @@
 public class HelloWorldMain {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from Main Repo");
+        System.out.println("Hello, World!! from Main Repo");
     }
 }
