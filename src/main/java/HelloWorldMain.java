3609760f1a887fa7a4b36eca1b02da71b8e4969b
colin-sonarsource <colin.mueller@sonarsource.com>
2024-07-10 16:22:02 +0200

    Update HelloWorldMain.java

--- src/main/java/HelloWorldMain.java
+++ src/main/java/HelloWorldMain.java
@@ -1,5 +1,12 @@
+import javax.crypto.spec.PBEParameterSpec;
+
 public class HelloWorldMain {
     public static void main(String[] args) {
         System.out.println("Hello, World!! from Main Repo");
+    }
+
+    public void hash() {
+        byte[] salt = "salty".getBytes();
+        PBEParameterSpec cipherSpec = new PBEParameterSpec(salt, 10000); // Noncompliant
     }
 }
