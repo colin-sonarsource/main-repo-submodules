0f9ab39cea992a045fa60e38e003893beae2770c
colin-sonarsource <colin.mueller@sonarsource.com>
2024-07-10 16:41:21 +0200

    Update HelloWorldMain.java

--- src/main/java/HelloWorldMain.java
+++ src/main/java/HelloWorldMain.java
@@ -2,4 +2,9 @@ public class HelloWorldMain {
     public static void main(String[] args) {
         System.out.println("Hello, World! from Main Repo");
     }
+
+  public void method() {
+      Object myObject = null;
+      System.out.println(myObject.toString()); // Noncompliant: myObject is null
+    }
 }
