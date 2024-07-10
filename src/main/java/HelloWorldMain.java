e7b11b3e75982cd6afa3604e1bf277fca63cd31d
colin-sonarsource <colin.mueller@sonarsource.com>
2024-07-10 16:44:28 +0200

    Update HelloWorldMain.java

--- src/main/java/HelloWorldMain.java
+++ src/main/java/HelloWorldMain.java
@@ -2,7 +2,7 @@ import javax.crypto.spec.PBEParameterSpec;
 
 public class HelloWorldMain {
     public static void main(String[] args) {
-        System.out.println("Hello, World!! from Main Repo");
+        System.out.println("Hello, World!!! from Main Repo");
     }
 
     public void hash() {
